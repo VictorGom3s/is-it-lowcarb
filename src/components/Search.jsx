bae721f902617e81221db50c2b4ba5dff5620bd7
Victor Sampaio <vsampaio@pm.me>
2020-08-13 15:41:49 -0300

    Implemented API calls to search for a food and a simple and honestly implecise algorithm to verify the food low-carbiness

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,8 +1,19 @@
 import React, { useState } from "react";
+import axios from "axios";
+
+const Axios = axios.create({
+  baseURL: "https://trackapi.nutritionix.com/v2",
+  headers: {
+    "x-app-id": "c937f8d2",
+    "x-remote-user-id": 0,
+    "x-app-key": "fbed950f6236f6164b21346cedeb6492",
+  },
+});
 
 const Search = () => {
-  const [showResult, setShowResult] = useState(true);
+  const [showResult, setShowResult] = useState(false);
   const [isLC, setIsLC] = useState("Yep!");
+  const [searchTerm, setSearchTerm] = useState("");
 
   const resultDiv = (
     <div className="result">
@@ -10,14 +21,38 @@ const Search = () => {
     </div>
   );
 
+  const handleSearch = async (e) => {
+    e.preventDefault();
+    try {
+      const { data: res } = await Axios.post("/natural/nutrients", {
+        query: `100 grams of ${searchTerm}`,
+      });
+      isItLowCarb(res.foods[0]);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const isItLowCarb = (food) => {
+    const netCarbs = food.nf_total_carbohydrate - food.nf_dietary_fiber;
+
+    if (netCarbs > 15 || food.nf_sugars > 5) setIsLC("Nope!");
+    else setIsLC("Yep!");
+
+    setShowResult(true);
+  };
+
   return (
     <section id="search">
-      <input
-        type="text"
-        name="search"
-        id="search"
-        placeholder="Search for a food"
-      />
+      <form onSubmit={handleSearch}>
+        <input
+          type="text"
+          name="search"
+          id="search"
+          placeholder="Search for a food and press enter"
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+      </form>
       {showResult ? resultDiv : ""}
     </section>
   );
