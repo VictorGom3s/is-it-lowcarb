0eb7082d913bf4ecd28277c8af4d1848a312355b
Victor Sampaio <vsampaio@pm.me>
2020-08-13 14:26:31 -0300

    Finishing the search result UI and logic

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,6 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Search = () => {
+  const [showResult, setShowResult] = useState(true);
+  const [isLC, setIsLC] = useState("Yep!");
+
+  const resultDiv = (
+    <div className="result">
+      <p>{isLC}</p>
+    </div>
+  );
+
   return (
     <section id="search">
       <input
@@ -9,6 +18,7 @@ const Search = () => {
         id="search"
         placeholder="Search for a food"
       />
+      {showResult ? resultDiv : ""}
     </section>
   );
 };
